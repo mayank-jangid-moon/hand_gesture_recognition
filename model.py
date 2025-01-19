75e578bd656052ba9b57907ce2403bbcc9e1bc3f
Mayank Jangid <mayank.jangid.moon@gmail.com>
2025-01-19 22:06:41 +0530

    data aa rha hai gais ek second mein 30 baar

--- model.py
+++ model.py
@@ -1,42 +1,70 @@
 import cv2
 import mediapipe as mp
+import pandas as pd
+import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+data = []
+
+frame_counter = 0
+
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-    max_num_hands=2) as hands:
+    min_detection_confidence=0.9,
+    min_tracking_confidence=0.9,
+    max_num_hands=1) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
       continue
-
-    # Flip the image horizontally for a later selfie-view display, and convert
+    
+    frame_counter += 1
+    # image = cv2.imread("sample_rr.png")
+    
+    
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     results = hands.process(image)
-    image_height, image_width, _ = image.shape
-    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
     if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
+      for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+        
+        
+        # print(f"\n\n\n\nMultihandedness: {results.multi_handedness[idx]}")
+        # print(f"\n\n\n\n Hand number {idx} : \n\n\n\n")
+
+        frame_data = {"Frame": frame_counter}
+
+        # break
         # Here is How to Get All the Coordinates
+
         for ids, landmark in enumerate(hand_landmarks.landmark):
-            print(ids, landmark)
-            # print (ids, landmark.x, landmark.y, landmark.z)
+          # print(ids, landmark)
+
+          frame_data[f"Landmark_{ids}_X"] = landmark.x
+          frame_data[f"Landmark_{ids}_Y"] = landmark.y
+          frame_data[f"Landmark_{ids}_Z"] = landmark.z
+          
+        data.append(frame_data)
+          
+          # print (ids, landmark.x, landmark.y, landmark.z)
         mp_drawing.draw_landmarks(image, 
                                   hand_landmarks, 
                                   mp_hands.HAND_CONNECTIONS, 
                                   mp_drawing_styles.get_default_hand_landmarks_style(), 
                                   mp_drawing_styles.get_default_hand_connections_style())
+
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
+
+df = pd.DataFrame(data)
+
+df.to_csv("thumbs_up.csv", index=True)
+print("CSV created sucessfully!!!")
 cap.release()
